9dbe61095a1c6b73115a3ee273e0a9fea1d35b51
Bram Hurkmans <b.hurkmans@spinnov.com>
2022-03-29 21:53:55 +0200

    finsihed up the endpoints of the commandsservice and platformservice

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Collections.Generic;
+using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandsService.Controllers
@@ -7,13 +11,27 @@ namespace CommandsService.Controllers
     [ApiController]
     public class PlatformsController : ControllerBase
     {
-        public PlatformsController()
+        private readonly ICommandRepo _commandRepo;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(ICommandRepo commandRepo, IMapper mapper)
         {
-            
+            _commandRepo = commandRepo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("Getting platforms from CommandsService");
+
+            var platformItems = _commandRepo.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
         }
 
         [HttpPost]
-        public ActionResult TestInboundConection()
+        public ActionResult TestInboundConnection()
         {
             Console.WriteLine("Inbound POST at the # command service!");
 
