784c30cedbc800f45657e821c3f5c1209b4341c5
Bram Hurkmans <b.hurkmans@spinnov.com>
2022-06-19 22:04:53 +0200

    changed frontend

--- hyves2-frontend/src/common/jwt.service.js
+++ hyves2-frontend/src/common/jwt.service.js
@@ -1,7 +1,7 @@
 const AUTH_TOKEN = "auth_token";
 
 export const getToken = () => {
-  return window.localStorage.getItem(AUTH_TOKEN);
+  return 'Bearer ' + getCookie('bearer_token')
 };
 
 export const saveToken = token => {
@@ -11,5 +11,21 @@ export const saveToken = token => {
 export const destroyToken = () => {
   window.localStorage.removeItem(AUTH_TOKEN);
 };
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
 
 export default { getToken, saveToken, destroyToken };
